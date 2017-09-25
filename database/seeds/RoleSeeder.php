94674d5a43ea32880ab75f244363a4690783ab25
gao <fishis@163.com>
2017-09-25 10:10:37 +0800

    merge

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'name' => 'institution'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'teacher'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'admin'
+        ]);
+        DB::table('roles')->insert([
+            'name' => 'operator'
+        ]);
+    }
+}
