89c5582228970a229c4ef5607b5af181588583c5
aj <bitmyth@outlook.com>
2017-11-15 17:03:19 +0800

    merge

--- vendor/laravel/framework/src/Illuminate/Auth/Passwords/DatabaseTokenRepository.php
+++ vendor/laravel/framework/src/Illuminate/Auth/Passwords/DatabaseTokenRepository.php
@@ -80,8 +80,7 @@ class DatabaseTokenRepository implements TokenRepositoryInterface
         // We will create a new, random token for the user so that we can e-mail them
         // a safe link to the password reset form. Then we will insert a record in
         // the database so that we can verify the token within the actual reset.
-//        $token = $this->createNewToken();
-        $token = $token ?: $this->createCustomToken();
+        $token = $token?:$this->createNewToken();
 
         $this->getTable()->insert($this->getPayload($email, $token));
 
