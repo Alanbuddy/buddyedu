d4b220a5ea0fe93dce177ff0632a5580ebf063eb
alan <wenqiang@buddyrobots.com>
2017-11-30 15:07:32 +0800

    admin3

--- public/js/agent-add.js
+++ public/js/agent-add.js
@@ -5,4 +5,36 @@ $(document).ready(function(){
   $(".close").click(function(){
     $("#addModal").modal("hide");
   });
+
+  $('#teacher-select').select2({
+  	placeholder: "请选择教师名"
+  });
+
+  $("#apply").click(function(){
+  	var course = $("#course").val();
+  	var point = $("#point").val();
+  	var teacher = $("#teacher-select").val();
+  	var num = $("#num").val();
+  	console.log(course);
+  	console.log(teacher);
+  	console.log(point);
+  	$.ajax({
+  	  url: window.course_store,
+  	  type: 'post',
+  	  data: {
+  	    _token: window.token,
+  	    course_id: course,
+  	    point_id: point,
+  	    teachers: teacher,
+  	    quota: num,
+  	    begin: "2017-12-23",
+  	    end: "2017-12-24"
+  	  },
+  	  success: function( data ) {
+  	    if(data.success){
+          location.href = window.schedule_create;
+        }
+  	  }
+  	});
+  });
 });
