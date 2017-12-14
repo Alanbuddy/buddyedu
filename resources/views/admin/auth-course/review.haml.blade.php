c083ba600e4c3f05bdf72c239f8cd08a5d05d7da
alan <wenqiang@buddyrobots.com>
2017-12-14 17:05:32 +0800

    auth1

--- resources/views/admin/auth-course/review.haml.blade.php
+++ resources/views/admin/auth-course/review.haml.blade.php
@@ -14,37 +14,35 @@
   .tab-title
     %ul.clearfix
       %li
-        %a.f14c{href: route('course.show', $course->id)} 课程信息
+        %a.f14c{href: route('courses.show', $course->id)} 课程信息
       %li
         %a.f14c{href: route('course.merchant', $course->id)} 授权机构(4)
       %li.f14a.bg16b 评论查看(16)
 
   .desc-div
-    // - if(count($items) == 0) 
-    //   .undiscover.f14
-    //     %img.undiscover-icon{src: "/icon/undiscover.png"}
-    // - else
-    .table-box
-      %table.table.table-hover.table-height
-        %thead.f14b.th-bg
-          %tr
-            %th 学生姓名
-            %th 机构名称
-            %th 评论内容
-            %th{colspan: 2} 操作
-        %tbody
-        -foreach($items as $item)
-          %tr
-            %td=$item->user->name
-            %td 某一机构名称
-            %td=$item->content
-            %td#green 展示
-            %td.f12e 隐藏
-            
-
-    .select-page 
-      %span.choice-page
-        != $items->links()
+    - if(count($items) == 0) 
+      .undiscover.f14
+        %img.undiscover-icon{src: "/icon/undiscover.png"}
+    - else
+      .table-box
+        %table.table.table-hover.table-height
+          %thead.f14b.th-bg
+            %tr
+              %th 学生姓名
+              %th 机构名称
+              %th 评论内容
+              %th{colspan: 2} 操作
+          %tbody
+          -foreach($items as $item)
+            %tr
+              %td=$item->user->name
+              %td 某一机构名称
+              %td=$item->content
+              %td#green 展示
+              %td.f12e 隐藏
+      .select-page 
+        %span.choice-page
+          != $items->links()
 
   
 @endsection
--- resources/views/admin/auth-course/show.haml.blade.php
+++ resources/views/admin/auth-course/show.haml.blade.php
@@ -14,35 +14,35 @@
   .tab-title
     %ul.clearfix
       %li
-        %a.f14c{href: route('course.show', $course->id)} 课程信息
+        %a.f14c{href: route('courses.show', $course->id)} 课程信息
       %li.f14a.bg16b 授权机构(4)
       %li
         %a.f14c{href: route('course.comment', $course->id)} 评论查看(16)
 
   .desc-div
-    // - if(count($items) == 0) 
-    //   .undiscover.f14
-    //     %img.undiscover-icon{src: "/icon/undiscover.png"}
-    // - else
-    .table-box
-      %table.table.table-hover.table-height
-        %thead.f14b.th-bg
-          %tr
-            %th 机构名称
-            %th 当前开设本课/历史开设本课
-            %th 当前报名人数/历史报名人数
-            %th 操作
-        %tbody
-          -foreach($items as $item)
+    - if(count($items) == 0) 
+      .undiscover.f14
+        %img.undiscover-icon{src: "/icon/undiscover.png"}
+    - else
+      .table-box
+        %table.table.table-hover.table-height
+          %thead.f14b.th-bg
             %tr
-              %td=$item->name
-              %td=$item->ongoingSchedulesCount.'/'.$item->schedules_count
-              %td=$item->ongoingStudentsCount.'/'.$item->studentsCount
-              %td.red 取消授权
+              %th 机构名称
+              %th 当前开设本课/历史开设本课
+              %th 当前报名人数/历史报名人数
+              %th 操作
+          %tbody
+            -foreach($items as $item)
+              %tr
+                %td=$item->name
+                %td=$item->ongoingSchedulesCount.'/'.$item->schedules_count
+                %td=$item->ongoingStudentsCount.'/'.$item->studentsCount
+                %td.red 取消授权
 
-    .select-page 
-      %span.choice-page
-        != $items->links()
+      .select-page 
+        %span.choice-page
+          != $items->links()
   
 @endsection
 
