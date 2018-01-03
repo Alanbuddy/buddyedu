22fdc56160be4cd421a4ca461580b3ed38ae6688
alan <wenqiang@buddyrobots.com>
2018-01-03 18:24:49 +0800

    mobile2

--- resources/views/mobile/student-course.haml.blade.php
+++ resources/views/mobile/student-course.haml.blade.php
@@ -5,15 +5,17 @@
 
 @section('content')
 .desc-div
-  %img.mine{src: "/icon/mobile/mine.png"}
+  %a{href: route('profile')}
+    %img.mine{src: "/icon/mobile/mine.png"}
   %p.f16.fb.title.mb48 李晓明的课程
   .items
+    // - foreach($items as $item)
     .item.mb48
       %img.course-icon{src: "/icon/mobile/product.jpg"}
       .title-div
         %p.f14.text-color.title-margin Buddy动物园
-        %p.f14.text-color 机构教学点设置的长长的名称
-      %a.click-div
+        %p.f14.text-color.point-name 机构教学点设置的长长的名称
+      %a.click-div{href: route('user.drawings', $item->schedule_id)}
         %span.f12.text-blue 我的作品
         %img.arrow{src: "/icon/mobile/more.png"}
     .item.mb48
