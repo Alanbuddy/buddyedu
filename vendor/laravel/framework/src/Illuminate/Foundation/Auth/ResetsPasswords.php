1b990d5f98aa0da683b8b6fa02c5ddf2ef0c6fb8
aj <bitmyth@outlook.com>
2018-01-03 14:38:05 +0800

    merge

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/ResetsPasswords.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/ResetsPasswords.php
@@ -51,6 +51,7 @@ trait ResetsPasswords
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
+        return $response;
         return $response == Password::PASSWORD_RESET
                     ? $this->sendResetResponse($response)
                     : $this->sendResetFailedResponse($request, $response);
