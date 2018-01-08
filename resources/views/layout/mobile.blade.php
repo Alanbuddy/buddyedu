1ff15a07b7b5de8914c005dcbaad6283a81d307f
alan <wenqiang@buddyrobots.com>
2018-01-08 11:34:07 +0800

    pay

--- resources/views/layout/mobile.blade.php
+++ resources/views/layout/mobile.blade.php
@@ -44,7 +44,47 @@
 <script src="/js/mobile-notification.js"></script>
 
 <script>
+    wx.config({
+        debug: false,
+        appId: '{{ $signPackage["appId"]}}',
+        timestamp: '{{ $signPackage["timestamp"]}}',
+        nonceStr: '{{ $signPackage["nonceStr"]}}',
+        signature: '{{ $signPackage["signature"]}}',
+        jsApiList: [
+            // 所有要调用的 API 都要加到这个列表中
+            'onMenuShareTimeline',
+            'scanQRCode',
+            'hideAllNonBaseMenuItem'
+        ]
+    });
+    wx.ready(function () {
+        // 在这里调用 API
+        //            wx.hideAllNonBaseMenuItem();
+        wx.onMenuShareTimeline({
+            title: '', // 分享标题
+            link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            imgUrl: '', // 分享图标
+            success: function () {
+                // 用户确认分享后执行的回调函数
+                alert('shared');
+            },
+            cancel: function () {
+                // 用户取消分享后执行的回调函数
+            }
+        });
+    })
 
+    var appId = "{{config('wechat.mp.app_id')}}";
+    var app_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
+        + appId
+        + "&redirect_uri=http%3a%2f%2f"
+        + "cloud.buddyrobots.com/wechat/login&response_type=code&scope=snsapi_userinfo&state=STATE&connect_redirect=1#wechat_redirect";
+
+    @if(!Auth::check())
+    if (navigator.userAgent.toLowerCase().match(/MicroMessenger/i) == 'micromessenger') {
+        location.href = app_url;
+    }
+    @endif
 </script>
 
 @yield('script')
