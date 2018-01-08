f1ea2ba6d014351a2e82c88f1bb4b45f227b0512
aj <bitmyth@outlook.com>
2018-01-08 15:30:29 +0800

    merge

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Wechat\PayNotifyCallBack;
 use App\Http\Wechat\WxApi;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
