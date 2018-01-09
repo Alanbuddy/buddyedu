0d789cec6acd0dc54c2ce0560f8c1ec1957b793e
aj <bitmyth@outlook.com>
2018-01-09 17:07:45 +0800

    merge

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -276,11 +276,10 @@ class ScheduleController extends Controller
     //报名成功
     public function enrolled(Schedule $schedule)
     {
-        $user = auth()->user();
-        $order = $this->getEnrollOrder($schedule);
-        return $order
-            ? $this->enroll($schedule, $user->id)
-            : ['success' => false, 'message' => 'no finished order found'];
+        return redirect(route('landing',$schedule));
+//        $user = auth()->user();
+//        $order = $this->getEnrollOrder($schedule);
+//        return $order ? $this->enroll($schedule, $user->id) : ['success' => false, 'message' => 'no finished order found'];
     }
 
     // /api/v1/schedules/sign-in?schedule_id=1&api_token=da262427-88c6-356a-a431-8686856c81b3&ordinal_no=1&merchant_id=1&point_id=1&students[]=1&students[]=2
