776528cbdca3c3567a8af550f638a2686b17bded
alan <wenqiang@buddyrobots.com>
2018-01-12 16:25:20 +0800

    storage3

--- resources/views/mobile/register-success.haml.blade.php
+++ resources/views/mobile/register-success.haml.blade.php
@@ -11,9 +11,10 @@
   %p.f14.text-color.mb40= '您已成功报名'.$schedule->course->name.'动物园课程!'
   %p.f14.text-color.mb40= '开课前一天我们将发送提醒到您的手机：'.auth()->user()->phone.',届时不要迟到哦！'
   %p.f14.text-color.mb40= '如有任何疑问，请致电客服热线'.$schedule->point->contact
-  %button.btn.click-btn.f14#next_btn{type: "button", "data-id" =>$schedule->id} 完成
+  %a{href: route('schedules.enrolled',$schedule)}
+    %button.btn.click-btn.f14#next_btn{type: "button"} 完成
 @endsection
 
 @section('script')
-<script src= "/js/mobile-register-over.js"></script>
+
 @endsection
