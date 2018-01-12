2c0a941932835c982a577330e80c55fa12ca900e
alan <wenqiang@buddyrobots.com>
2018-01-12 16:37:42 +0800

    storage5

--- public/js/mobile-register-over.js
+++ public/js/mobile-register-over.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $("#next_btn").click(function(){
+    var schedule_id = $(this).attr("data-id");
     location.href = window.register_end.replace(/-1/, schedule_id);
   });
 });
