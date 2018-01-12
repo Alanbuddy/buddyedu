39312cc17e9d69dcfbe1a50f13784b8bec18a2d5
alan <wenqiang@buddyrobots.com>
2018-01-12 16:22:10 +0800

    storage2

--- resources/views/mobile/register-success.haml.blade.php
+++ resources/views/mobile/register-success.haml.blade.php
@@ -8,10 +8,10 @@
 @section('content')
 .desc-div
   %p.f20.fb.title.mb72 报名成功
-  %p.f14.text-color.mb40 您已成功报名Buddy动物园课程!
-  %p.f14.text-color.mb40 开课前一天我们将发送提醒到您的手机：13211223344,届时不要迟到哦！
-  %p.f14.text-color.mb40 如有任何疑问，请致电客服热线010-1232324
-  %button.btn.click-btn.f14#next_btn{type: "button"} 完成
+  %p.f14.text-color.mb40= '您已成功报名'.$schedule->course->name.'动物园课程!'
+  %p.f14.text-color.mb40= '开课前一天我们将发送提醒到您的手机：'.auth()->user()->phone.',届时不要迟到哦！'
+  %p.f14.text-color.mb40= '如有任何疑问，请致电客服热线'.$schedule->point->contact
+  %button.btn.click-btn.f14#next_btn{type: "button", "data-id" =>$schedule->id} 完成
 @endsection
 
 @section('script')
