4427b16489aa124fdac8ac201c2927b980480898
alan <wenqiang@buddyrobots.com>
2018-01-16 18:05:08 +0800

    qrcode

--- resources/views/mobile/product-list.haml.blade.php
+++ resources/views/mobile/product-list.haml.blade.php
@@ -7,16 +7,16 @@
 .desc-div
   %p.f16.fb.title.mb60=auth()->user()->name.'的作品'
   .product-list
-    // - if(count($items) == 0)
-    //   .undiscover
-    //     %img.undiscover-icon{src: "/icon/undiscover.png"}
-    // - else
-    -foreach($items as $item)
-      .item
-        %p.f14.date.mb16=$item->created_at
-        .img-div
-          %a{href: route('user.drawings.show',$item->id)}
-            %img{src: $item->path}
+    - if(count($items) == 0)
+      .undiscover
+        %img.undiscover-icon{src: "/icon/undiscover.png"}
+    - else
+      -foreach($items as $item)
+        .item
+          %p.f14.date.mb16=$item->created_at
+          .img-div
+            %a{href: route('user.drawings.show',$item->id)}
+              %img{src: $item->path}
             
 @endsection
 
