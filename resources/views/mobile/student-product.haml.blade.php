94d0e4101a8d4d1ed1e2d3be11e08a77b1268b0f
alan <wenqiang@buddyrobots.com>
2018-01-16 10:33:18 +0800

    student1

--- resources/views/mobile/student-product.haml.blade.php
+++ resources/views/mobile/student-product.haml.blade.php
@@ -9,7 +9,7 @@
   %p.f12.date.mb40 2017/12/21
   %img.product.mb40{src: "/icon/mobile/product.jpg"}
   %video.product.mb80{src: ""}
-  %p.f14.title 玩伴画的小动物“活”了，小朋友们一起来体验吧!
+  %p.f14.title 我画的小动物“活”了，小玩伴们一起来体验吧!
 @endsection
 
 @section('script')
