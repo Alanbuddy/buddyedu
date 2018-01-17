882175f22e5559a798801ed78635f0419ac7579a
alan <wenqiang@buddyrobots.com>
2018-01-17 16:49:48 +0800

    info8

--- public/js/mobile-course-show.js
+++ public/js/mobile-course-show.js
@@ -81,16 +81,11 @@ $(document).ready(function(){
           'signType': 'MD5', //微信签名方式：
           'paySign': ''+signPackage.sign,
       }, function (res) {
-          if (res.err_msg == 'get_brand_wcpay_request:ok') {
-              if(person_phone == "未注册"){
-                alert("111");
-                location.href = window.user_phone;
-              }else{
-                alert("aaa");
-                location.href = window.pay_finish;
-              }
-          } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-
+          if (res.err_msg == 'get_brand_wcpay_request:ok' && person_phone == "未注册") {
+            location.href = window.user_phone;
+          }else if(res.err_msg == 'get_brand_wcpay_request:ok' && person_phone != "未注册") {
+            location.href = window.pay_finish;
+          }
       });
   }
 
