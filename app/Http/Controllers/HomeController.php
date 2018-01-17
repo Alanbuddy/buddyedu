02f10282169e190c8f9c399af41be7d96b6473af
aj <bitmyth@outlook.com>
2018-01-17 16:34:04 +0800

    merge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,7 @@ class HomeController extends Controller
             $isFull = $this->isFull($schedule);
             $available = $this->available($schedule);
         }
-        $hasCommented = Comment::where('user_id', $user->id)->where('schedule_id', $schedule->id)->count();
+        $hasCommented = Comment::where('user_id', $user->id)->where('schedule_id', $schedule)->count();
         return view('mobile.course-show', compact('schedule', 'hasEnrolled', 'isFull', 'available', 'user','hasCommented'));
     }
 
