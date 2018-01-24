bba290d5b81d9f854a7ce15cd1dba9be195d695b
alan <wenqiang@buddyrobots.com>
2018-01-24 14:33:39 +0800

    info

--- resources/views/mobile/info.haml.blade.php
+++ resources/views/mobile/info.haml.blade.php
@@ -7,7 +7,7 @@
   window.landing = "#{route('landing',$schedule??1)}"
   window.sms_send = "#{route('sms.send')}"
   window.validmobile = "#{route('validate.phone')}"
-  window.login = "#{route('login')}"
+  window.register = "#{route('register')}"
 @endsection
 
 @section('content')
