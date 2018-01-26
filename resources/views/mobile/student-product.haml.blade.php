895132d36f816a650661a7206cadd3edcfd1c67b
alan <wenqiang@buddyrobots.com>
2018-01-26 14:39:30 +0800

    video

--- resources/views/mobile/student-product.haml.blade.php
+++ resources/views/mobile/student-product.haml.blade.php
@@ -9,7 +9,7 @@
   %p.f12.date.mb40=$drawing->created_at
   %img.product.mb40{src: $drawing->path }
   -if($video)
-    %video.product.mb80{src: $video->path }
+    %video.product.mb80{src: $video->path, controls: "controls" }
   %p.f14.title 我画的小动物“活”了，小玩伴们一起来体验吧!
 @endsection
 
