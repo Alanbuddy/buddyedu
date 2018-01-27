9764b465d043b48f8e469dddc7965f04c249d52d
aj <bitmyth@outlook.com>
2018-01-27 15:15:59 +0800

    merge

--- app/Http/Wechat/PayNotifyCallBack.php
+++ app/Http/Wechat/PayNotifyCallBack.php
@@ -59,7 +59,8 @@ class PayNotifyCallBack extends WxPayNotify
         if ($order) {
             $schedule = Schedule::find($order->product_id);
             DB::transaction(function () use ($data, $order, $schedule) {
-                $order->fill(['wx_transaction_id ' => $data["transaction_id"],
+                $order->fill([
+                    'wx_transaction_id' => $data["transaction_id"],
                     'wx_total_fee' => $data["total_fee"],
                     'status' => 'paid',
                     'proportion'=>$schedule->course->proportion
