f76e9177157938c3354125f1c01665999648b05f
alan <wenqiang@buddyrobots.com>
2018-02-02 17:00:14 +0800

    point7

--- resources/views/mobile/edu-point.haml.blade.php
+++ resources/views/mobile/edu-point.haml.blade.php
@@ -10,17 +10,17 @@
   %p.f20.fb.title 附近的教学点
   .location-map.mb60#container
   .item-div.text-color
-    .item.clearfix.mb60
-      %p.f18.fl.fb 1
-      .point-div
-        .point-caption.f18.fb 教学点名称教学点名称教学点名称教学点名称教学点名称教学点名称
-        .content-span.f14
-          %span 联系方式:
-          %span 132444444444
-        .content-span.f14
-          %span 详细地址:
-          %span 北京市朝阳区安立路
-        %span.distance.f14 2.1km
+    // .item.clearfix.mb60
+    //   %p.f18.fl.fb 1
+    //   .point-div
+    //     .point-caption.f18.fb 教学点名称教学点名称教学点名称教学点名称教学点名称教学点名称
+    //     .content-span.f14
+    //       %span 联系方式:
+    //       %span 132444444444
+    //     .content-span.f14
+    //       %span 详细地址:
+    //       %span 北京市朝阳区安立路
+    //     %span.distance.f14 2.1km
 
 @endsection
 
