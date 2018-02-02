d3a102182961cf6eea2b6ae38f21519e5494c0c5
alan <wenqiang@buddyrobots.com>
2018-02-02 10:46:39 +0800

    https

--- resources/views/layout/mobile.blade.php
+++ resources/views/layout/mobile.blade.php
@@ -90,7 +90,7 @@
     var appId = "{{config('wechat.mp.app_id')}}";
     var app_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="
         + appId
-        + "&redirect_uri=http%3a%2f%2f"
+        + "&redirect_uri=https%3a%2f%2f"
         + "cloud.buddyrobots.com/wechat/login&response_type=code&scope=snsapi_userinfo"
         + "&state="+location.href
         + "&connect_redirect=1#wechat_redirect";
