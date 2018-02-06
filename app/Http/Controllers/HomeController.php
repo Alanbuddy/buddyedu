059e01c2590fc87e985da19f778f9f5450cec9a7
aj <bitmyth@outlook.com>
2018-02-06 17:09:38 +0800

    merge

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,9 +45,13 @@ class HomeController extends Controller
 		    ->with('point')
 		    ->withCount('teachers')
 		    ->withCount('students')
-		    ->orderByDesc('id')
-		    ->paginate();
-        //dd($items);
+		    ->orderByDesc('id');
+	    if($user=auth()->user()){
+		    $items ->addSelect(DB::Raw("(select count(*) from schedule_user where schedule_user.schedule_id=schedules.id and type='student' and schedule_user.user_id=$user->id) as attended"));
+	    }
+		    
+	    $items=$items->paginate();
+        dd($items);
 	    //return redirect(route('schedules.index'));
 	return view('mobile.course-list',compact('items'));
     }
