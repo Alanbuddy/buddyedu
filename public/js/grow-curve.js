8254955af0fd0de0f2384ccb75b673e203915318
alan <wenqiang@buddyrobots.com>
2018-02-06 16:04:14 +0800

    course-list4

--- public/js/grow-curve.js
+++ public/js/grow-curve.js
@@ -1,79 +1,51 @@
 $(document).ready(function(){
   $('#curve').highcharts({
     chart: {
-          zoomType: 'x'
-      },
-      title: {
-          text: '美元兑欧元汇率走势图'
-      },
-      subtitle: {
-          text: document.ontouchstart === undefined ?
-          '鼠标拖动可以进行缩放' : '手势操作进行缩放'
-      },
-      xAxis: {
-          type: 'datetime',
-          dateTimeLabelFormats: {
-              millisecond: '%H:%M:%S.%L',
-              second: '%H:%M:%S',
-              minute: '%H:%M',
-              hour: '%H:%M',
-              day: '%m-%d',
-              week: '%m-%d',
-              month: '%Y-%m',
-              year: '%Y'
-          }
-      },
-      tooltip: {
-          dateTimeLabelFormats: {
-              millisecond: '%H:%M:%S.%L',
-              second: '%H:%M:%S',
-              minute: '%H:%M',
-              hour: '%H:%M',
-              day: '%Y-%m-%d',
-              week: '%m-%d',
-              month: '%Y-%m',
-              year: '%Y'
-          }
-      },
-      yAxis: {
-          title: {
-              text: '汇率'
-          }
-      },
-      legend: {
-          enabled: false
-      },
-      plotOptions: {
-          area: {
-              fillColor: {
-                  linearGradient: {
-                      x1: 0,
-                      y1: 0,
-                      x2: 0,
-                      y2: 1
-                  },
-                  stops: [
-                      [0, Highcharts.getOptions().colors[0]],
-                      [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                  ]
-              },
-              marker: {
-                  radius: 2
-              },
-              lineWidth: 1,
-              states: {
-                  hover: {
-                      lineWidth: 1
-                  }
-              },
-              threshold: null
-          }
-      },
-      series: [{
-          type: 'area',
-          name: '曲线',
-          data: data
-      }]
+            zoomType: 'x'
+        },
+        title: {
+            text: '2017年环法德法国第八站：Dole站'
+        },
+        xAxis: {
+            labels: {
+                format: '{value} km'
+            },
+            minRange: 5,
+            title: {
+                text: '距离'
+            }
+        },
+        yAxis: {
+            startOnTick: true,
+            endOnTick: false,
+            maxPadding: 0.35,
+            title: {
+                text: null
+            },
+            labels: {
+                format: '{value} m'
+            }
+        },
+        tooltip: {
+            headerFormat: '距离: {point.x:.1f} km<br>',
+            pointFormat: '海拔：{point.y} m ',
+            shared: true,
+            enabled: false
+        },
+        legend: {
+            enabled: false
+        },
+        series: [{
+            data: elevationData,
+            lineColor: Highcharts.getOptions().colors[1],
+            color: Highcharts.getOptions().colors[2],
+            fillOpacity: 0,
+            name: '海拔',
+            marker: {
+                enabled: false
+            },
+            threshold: null
+        }]
   });
 
 
