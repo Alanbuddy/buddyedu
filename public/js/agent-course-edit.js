b71e5c2ab2348c7e487fd66208fba5f5ceeac76a
alan <wenqiang@buddyrobots.com>
2018-02-13 10:05:15 +0800

    agent course  modify

---
+++ public/js/agent-course-edit.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+  var is_batch = $(".is-batch").text();
+  $("#modify").click(function(){
+    if(is_batch == 1){
+      $("#course-price").hide();
+      $("#course-num").hide();
+      $("#course-time").hide();
+      $("#editModal").find(".modal-dialog").css({"margin-top":"-282px","height": "564px"});
+    }
+    $("#editModal").modal("show");
+  });
+  $(".close").click(function(){
+    $("#editModal").modal("hide");
+  });
+
+  $('#teacher-select').select2({
+    placeholder: "请选择教师名"
+  });
+
+  $( "#datepicker1" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange : '-70:+10'
+      });
+  $( "#datepicker1" ).datepicker( $.datepicker.regional[ "zh-TW" ] );
+  $( "#datepicker1" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+
+  $( "#datepicker2" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        yearRange : '-70:+10'
+      });
+  $( "#datepicker2" ).datepicker( $.datepicker.regional[ "zh-TW" ] );
+  $( "#datepicker2" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
+
+
+});
--- resources/views/agent/course/course-info.haml.blade.php
+++ resources/views/agent/course/course-info.haml.blade.php
@@ -1,7 +1,11 @@
 @extends('layout.agent')
 @section('css')
+<link rel="stylesheet" href="/css/select2.min.css">
 <link rel="stylesheet" href="{{ mix('/css/class_info.css') }}">
+<link rel="stylesheet" href="/css/jquery-ui.min.css">
 
+:javascript
+  window.course_select = "#{route('courses.index')}"
 @endsection
 
 @section('content')
@@ -23,6 +27,7 @@
       .name-div
         %p.f24b= $item->course->name
         %p.f12a.mt16= $item->point->name
+        %p.is-batch.hidden= $item->is_batch
       - if(!$item->is_batch)
         .money-div
           %span.f24c.mr8= $item->course->price ? "￥".$item->course->price : "暂无价格"
@@ -54,56 +59,60 @@
       .p-div
         %span.left-span 课程介绍：
         %span.right-span= $item->course->description
+      .p-div
+        %btn.modify-btn-width.fr#modify{"data-id" => $item->course->id} 修改
 
-// #editModal.modal.fade{"aria-hidden" => "true", "aria-labelledby" => "myModalLabel", :role => "dialog", :tabindex => "-1"} 
-//   .modal-dialog
-//     .modal-content
-//       .modalheader
-//         %img.close{"aria-hidden" => "true", "data-dismiss" => "modal", src: "/icon/close.png"}
-//       .modal-body
-//         %p.f24b.add-c 添加开课
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 开设课程:
-//           %select.form-control.input-width.f14d#course
-//             %option 请选择
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 教学点:
-//           %select.form-control.input-width.manager.f14d#point{:type => "text"} 
-//             - foreach($merchant->points()->where('approved',1)->get() as $item)
-//               %option{value: $item->id}= $item->name
-//         .controls.controls-row#course-price.mb24
-//           %label.input-caption.f14d 课程定价:
-//           %input.form-control.input-width.f14d#price{:type => "text"}
-//         .controls.controls-row#desc
-//           %label.input-caption.f14d.unvisible 故意隐藏:
-//           %span.hide-notice.mtb#course-desc
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 课程次数:
-//           %input.form-control.input-width.f14d#lessons-count{:type => "text"}
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d.teacher 授课老师:
-//           %select.form-control.input-width#teacher-select.f14d{multiple: "multiple"}
-//             - foreach($merchant->teachers as $item)
-//               %option{value: $item->id}= $item->name
-//         .controls.controls-row.mb24#course-num
-//           %label.input-caption.f14d 班级人数:
-//           %input.form-control.input-width.f14d#num{:type => "text"}
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 开课时间:
-//           %input.form-control.input-width#datepicker1{:type => "text"}
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 结课时间:
-//           %input.form-control.input-width#datepicker2{:type => "text"}
-//         .controls.controls-row.mb24#course-time
-//           %label.input-caption.f14d.time 详细时间:
-//           %textarea.form-control.input-width.f14d#time{:type => "text"}
-//         .controls.controls-row.mb24
-//           %label.input-caption.f14d 申请备注:
-//           %input.form-control.input-width.f14d#remark{:type => "text", placeholder: "非必填"}
-//         .btn-div     
-//           %btn.f16d.add-btn-width#apply 提交申请
+#editModal.modal.fade{"aria-hidden" => "true", "aria-labelledby" => "myModalLabel", :role => "dialog", :tabindex => "-1"} 
+  .modal-dialog
+    .modal-content
+      .modalheader
+        %img.close{"aria-hidden" => "true", "data-dismiss" => "modal", src: "/icon/close.png"}
+      .modal-body
+        %p.f24b.add-c 修改课程
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 开设课程:
+          %span.input-width.f14d#course= $item->course->name
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 教学点:
+          %select.form-control.input-width.manager.f14d#point{:type => "text"} 
+            - foreach($item->merchant->points()->where('approved',1)->get() as $item)
+              %option{value: $item->id}= $item->name
+        .controls.controls-row#course-price.mb24
+          %label.input-caption.f14d 课程定价:
+          %input.form-control.input-width.f14d#price{:type => "text", value: $item->course->price??"暂无"}
+        .controls.controls-row#desc
+          %label.input-caption.f14d.unvisible 故意隐藏:
+          %span.hide-notice.mtb#course-desc
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 课程次数:
+          %input.form-control.input-width.f14d#lessons-count{:type => "text", value: $item->course->lessons_count}
+        .controls.controls-row.mb24
+          %label.input-caption.f14d.teacher 授课老师:
+          %select.form-control.input-width#teacher-select.f14d{multiple: "multiple"}
+            - foreach($item->merchant->teachers as $item)
+              %option{value: $item->id}= $item->name
+        .controls.controls-row.mb24#course-num
+          %label.input-caption.f14d 班级人数:
+          %input.form-control.input-width.f14d#num{:type => "text", value: $item->quota??"暂无"}
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 开课时间:
+          %input.form-control.input-width#datepicker1{:type => "text", value: $item->begin}
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 结课时间:
+          %input.form-control.input-width#datepicker2{:type => "text", value: $item->end}
+        .controls.controls-row.mb24#course-time
+          %label.input-caption.f14d.time 详细时间:
+          %textarea.form-control.input-width.f14d#time{:type => "text", value: $item->time??"暂无"}
+        .controls.controls-row.mb24
+          %label.input-caption.f14d 申请备注:
+          %input.form-control.input-width.f14d#remark{:type => "text", placeholder: "非必填"}
+        .btn-div     
+          %btn.f16d.add-btn-width#apply 确定
 @endsection
 
 @section('script')
-<script src= "/js/agent-add.js"></script>
+<script src= "/js/select2.min.js"></script>
+<script src="/js/jquery-ui.min.js"></script>
+<script src="/js/datepicker-zh-TW.js"></script>
+<script src= "/js/agent-course-edit.js"></script>
 @endsection
