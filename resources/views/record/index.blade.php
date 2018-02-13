741edf77ad8d112c8643e99245e19e597768c5d0
aj <bitmyth@outlook.com>
2018-02-13 16:34:37 +0800

    merge

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -34,8 +34,12 @@
                 {{$item->merchant_id}}
             </td>
             <td>
-                {{substr($item->result,0,10)}}
+                {!! substr($item->result,0,10) !!}
+            </td>
+            <td>
+                {!! substr($item->result,0,10) !!}
             </td>
         </tr>
     @endforeach
 </table>
+{!! $item->render() !!}
